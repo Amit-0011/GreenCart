c1f9da15e3de3e89270eb875746337b7aa484630
Amit Kumar <142155353+Amit-0011@users.noreply.github.com>
2026-01-13 14:14:53 +0530

    render frontend url added

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://greencart-zeta-coral.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-zeta-coral.vercel.app', 'https://greencart-frontend-g4az.onrender.com']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
